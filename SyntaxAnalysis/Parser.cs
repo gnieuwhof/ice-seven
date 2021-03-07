0ae6be1640f12c8ff436892049e870c46f08a1df
Guido Nieuwhof <g.nieuwhofg@viadata.nl>
2021-03-07 03:50:02 +0100

    Added syntax analysis projects.

---
+++ SyntaxAnalysis/Parser.cs
@@ -0,0 +1,14 @@
+﻿namespace SyntaxAnalysis
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Parser
+    {
+        public void CreateAst()
+        {
+
+        }
+    }
+}
---
+++ SyntaxAnalysisTests/ParserTests.cs
@@ -0,0 +1,10 @@
+﻿namespace SyntaxAnalysisTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class ParserTests
+    {
+    }
+}
